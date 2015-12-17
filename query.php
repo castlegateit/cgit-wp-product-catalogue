54435c4ab6eacaf36560db01f1740a97f12c8580
John Hughes <john@castlegateit.co.uk>
2015-12-17 17:21:03 +0000

    Amend default order and pagination

--- query.php
+++ query.php
@@ -43,6 +43,38 @@ add_filter('pre_get_posts', function($query) {
         $query->set('meta_key', 'price');
     }
 
+    // Allow separate product pagination. By default, the number of products
+    // per page is taken from the WordPress settings.
+    if (defined('CGIT_PRODUCT_PER_PAGE')) {
+        $query->set('posts_per_page', CGIT_PRODUCT_PER_PAGE);
+    }
+
     // Return query
+    return $query;
+});
+
+/**
+ * Default sort order and pagination
+ *
+ * This function sets the default sort order of products in the main product
+ * archive. This does not affect product searches. The default order is:
+ * featured first, then sorted alphabetically by name.
+ */
+add_filter('pre_get_posts', function($query) {
+
+    // Filter only affects main product archive
+    if (!is_search() && !is_post_type_archive(CGIT_PRODUCT_POST_TYPE)) {
+        return;
+    }
+
+    // Set default sort order: featured first, then alphabetical
+    $order = array(
+        'meta_value_num' => 'DESC',
+        'title' => 'ASC',
+    );
+
+    $query->set('orderby', $order);
+    $query->set('meta_key', 'featured');
+
     return $query;
 });
